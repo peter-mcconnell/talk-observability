eb3185dc158cfee3a9fb5da1a63da40473e31990
Peter McConnell <me@petermcconnell.com>
2018-06-06 21:57:06 +0100

    updating chaos script to reflect repo name

--- chaos.sh
+++ chaos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash - 
 
-docker exec -i $(docker ps --filter "name=talkobservability_app1_" -q) sh -c "echo 'corrupting network ...'; tc qdisc add dev eth0 root netem corrupt 50%; echo 'done...'"
+docker exec -i $(docker ps --filter "name=talk-observability_app1_" -q) sh -c "echo 'corrupting network ...'; tc qdisc add dev eth0 root netem corrupt 50%; echo 'done...'"
